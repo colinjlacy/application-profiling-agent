947a46f94b72e0fda577d8b4a9b6eb03cb6f6733
Colin Lacy <colacy@cisco.com>
2025-11-27 06:38:21 -0500

    fixes c compile issue

--- agent/bpf/pqexec.bpf.c
+++ agent/bpf/pqexec.bpf.c
@@ -1,10 +1,21 @@
 // agent/bpf/pqexec.bpf.c
 
+// Define target architecture for PT_REGS macros
+#define __TARGET_ARCH_arm64
+
 // Minimal kernel-style typedefs so bpf_helper_defs.h is happy.
 typedef unsigned char __u8;
 typedef unsigned short __u16;
 typedef unsigned int __u32;
 typedef unsigned long long __u64;
+typedef signed char __s8;
+typedef signed short __s16;
+typedef signed int __s32;
+typedef signed long long __s64;
+typedef __u16 __be16;
+typedef __u32 __be32;
+typedef __u64 __be64;
+typedef unsigned int __wsum;
 
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>  // PT_REGS_PARM* macros
